a4eb8e42f50ef07718864ebacfadb42fec55139e
RitikBora <ritik.bora@xecurify.com>
2024-05-22 15:46:43 +0530

    popups added

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,30 +1,41 @@
+import React from 'react';
 
 export function Appbar() {
-
   return (
-    <div className='flex justify-between pt-2'> 
-        <div className="flex pl-48 mb-16">
-            <img src="bora_logo.png" alt="BoraLogo" width='75px' height='75px' />
-            <div className="text-lg font-bold pt-5">
-                Ritik
+    <div className='flex justify-between pt-2 relative'> 
+      <div className="flex pl-48">
+        <img src="bora_logo.png" alt="BoraLogo" width='75px' height='75px' />
+        <a href="https://www.linkedin.com/in/ritikbora" target="_blank" className="flex">
+          <div className="text-lg font-bold pt-5">
+            Ritik
+          </div>
+          <div className="text-lg pt-5 pl-1">
+            Bora
+          </div>
+        </a>
+      </div>
+      <div className="flex justify-between w-1/3 pt-5 relative">
+        <a className="font-semibold hover:font-bold cursor-pointer" target="_blank" href="https://github.com/RitikBora">
+          Github
+        </a>
+        <div className="font-semibold hover:font-bold cursor-pointer relative group">
+          <div className="group">
+            Projects
+            <div className="absolute top-full left-0 bg-white text-black px-4 py-2 rounded-lg hidden group-hover:block z-10">
+              Projects Popup
             </div>
-            <div className="text-lg  pt-5 pl-1">
-                Bora
-            </div>
+          </div>
         </div>
-        <div className="flex justify-between w-1/3 pt-5">
-            <div className="font-semibold hover:font-bold cursor-pointer">
-              Home 
+        <div className="font-semibold hover:font-bold cursor-pointer relative group">
+          <div className="group">
+            Contact
+            <div className="absolute top-full left-0 bg-white text-black px-4 py-2 rounded-lg hidden group-hover:block z-10">
+              Contacts Popup
             </div>
-            <div className="font-semibold hover:font-bold cursor-pointer">
-              Projects
-            </div>
-            <div className="font-semibold hover:font-bold cursor-pointer">
-              Contact
-            </div>
+          </div>
         </div>
-        <div> </div>
+      </div>
+      <div> </div>
     </div>  
   )
 }
-
--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,7 +1,7 @@
 export function Landing() {
 
   return (
-    <div className="ml-48 pb-20">
+    <div className="ml-48 pb-20 mt-16">
         <img src="hi_there.png" alt="Hello There"  />
         <div className="flex">
             <div className="m-4">
@@ -17,9 +17,9 @@ export function Landing() {
             </div>
         
         </div>
-        <div className=" absolute bottom-0 right-0 overflow-hidden">
-            <div className="relative -top-10 -right-10">
-                <img src="profilePhoto.png" alt="" width="800px" height="750px" className=""/>
+        <div className=" absolute top-20 bottom-0 right-0 overflow-hidden  mr-20">
+            <div className="relative top-20 right-20">
+                <img src="profilePhoto.png" alt="" width="450px" height="450px" className=""/>
             </div>
         </div>
 
